5e63de18b42eb1278a1e36e8d76206b206728cb5
H82M19\Student <Student>
2026-04-17 07:39:37 +0700

    Ex01

---
+++ Ex01/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Trang chủ (Home)</h1>
+      <p>Chào mừng bạn đến với trang chủ!</p>
+    </div>
+  );
+};
+
+export default Home;
